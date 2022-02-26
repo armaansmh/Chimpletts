78a54c2bb59cbcbb001ba39cea1fa1ee0197b6c9
Armaan <anthracene9@protonmail.com>
2022-02-26 23:11:26 +0530

    fixed discord link

--- src/Components/Copyright.jsx
+++ src/Components/Copyright.jsx
@@ -10,7 +10,7 @@ export default function Copyright() {
       className="h-[20vh] border-t border-white w-full text-white text-xl flex flex-col items-center justify-center gap-3"
     >
       <div className="flex gap-6 items-center  justify-center">
-        <a href="https://discord.gg/JdPr6gHbhv" target="_blank">
+        <a href="https://discord.gg/4UevgGHjpQ" target="_blank">
           <i className="fa-brands fa-discord text-3xl text-white hover:scale-125 transition-all"></i>
         </a>
         <a href="https://www.twitter.com/TheChimpetts/" target="_blank">
