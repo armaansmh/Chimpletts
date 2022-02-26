8e603e3d175742c8df01083e08d9b5ddf951fa19
Armaan <anthracene9@protonmail.com>
2022-02-26 18:03:40 +0530

    Fixed some carousel shit

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useRef, useEffect } from "react";
+import gsap from "gsap";
 import Image0 from "../Assets/NFTs/0.png";
 import Image1 from "../Assets/NFTs/1.png";
 import Image2 from "../Assets/NFTs/2.png";
@@ -10,6 +11,36 @@ import Image7 from "../Assets/NFTs/7.png";
 import "./Carousel.scss";
 
 export default function Carousel() {
+  const [currentIndex, setIndex] = useState(0);
+  const currentImage = useRef(null);
+  const currentImage2 = useRef(null);
+  const swipeLeft = () => {
+    if(currentIndex == 6) {
+      gsap.to(currentImage.current, {opacity: 0}).duration(0.4).then(() => {
+        setIndex(0)
+        gsap.to(currentImage.current, {opacity: 1})
+      })
+    } else {
+      gsap.to(currentImage.current, {opacity: 0}).duration(0.4).then(() => {
+        setIndex(currentIndex + 1);
+        gsap.to(currentImage.current, {opacity: 1})
+      })
+    }
+  }
+  const swipeRight = () => {
+
+    if(currentIndex == 0) {
+      gsap.to(currentImage2.current, {opacity: 0}).duration(0.4).then(() => {
+        setIndex(6)
+        gsap.to(currentImage2.current, {opacity: 1})
+      })
+    } else {
+      gsap.to(currentImage2.current, {opacity: 0}).duration(0.4).then(() => {
+        setIndex(currentIndex - 1);
+        gsap.to(currentImage2.current, {opacity: 1})
+      })
+    }
+  }
   const [images, setImages] = useState([
     Image1,
     Image2,
@@ -19,54 +50,28 @@ export default function Carousel() {
     Image6,
     Image7,
   ]);
-  const items = useRef(null);
-  console.log(images[0]);
-
-  useEffect(() => {
-    const slider = items.current;
-    let isDown = false;
-    let startX;
-    let scrollLeft;
-
-    slider.addEventListener("mousedown", (e) => {
-      isDown = true;
-      slider.classList.add("active");
-      startX = e.pageX - slider.offsetLeft;
-      scrollLeft = slider.scrollLeft;
-    });
-    slider.addEventListener("mouseleave", () => {
-      isDown = false;
-      slider.classList.remove("active");
-    });
-    slider.addEventListener("mouseup", () => {
-      isDown = false;
-      slider.classList.remove("active");
-    });
-    slider.addEventListener("mousemove", (e) => {
-      if (!isDown) return;
-      e.preventDefault();
-      const x = e.pageX - slider.offsetLeft;
-      const walk = (x - startX) * 3; //scroll-fast
-      slider.scrollLeft = scrollLeft - walk;
-      console.log(walk);
-    });
-  }, []);
   return (
-
-    <ul
-      style={{
-          cursor: 'grab'
-      }}
-      ref={items}
-      className="carrousel flex drag gap-4 overflow-x-auto w-[100%] max-w-[869px]  h-[100%] snap-x snap-mandatory before:shrink-0 before:w-[5vw] after:shrink-0 after:w-[5vw]"
-    >
-      {images.map((image, index) => {
-        return (
-          <li className="shrink-0 h-[500px] rounded-3xl">
-            <img src={image} className="w-[380px] max-w-[400px] rounded-3xl" alt="" />
-          </li>
-        );
-      })}
-    </ul>
+    <div className="flex w-[80%] max-w-[700px] relative overflow-x-hidden gap-4">
+      <div className="carousel-item h-[100%] w-[100%] max-w-[400px] max-h-[400px]">
+        <img src={images[currentIndex]} className="current-image" ref={currentImage} className="w-full h-full" alt="" /> 
+      </div>
+      <div className="hidden md:block carousel-item h-[100%] w-[100%] max-w-[400px] max-h-[400px]">
+        <img src={images[currentIndex == 6 ? 0 : currentIndex + 1]} ref={currentImage2} className="current-image2 w-full h-full" alt="" /> 
+      </div>
+      <div style={{
+        backgroundColor: 'rgba(0, 0, 0, 0.6)'
+      }} onClick={() => {
+        swipeLeft()
+      }} className="left-arrow absolute h-full flex flex-col items-center justify-center w-[15%] top-0 left-0">
+        <i className="fa-solid fa-arrow-left text-white text-5xl align-middle"></i>
+      </div>
+      <div style={{
+        backgroundColor: 'rgba(0, 0, 0, 0.6)'
+      }} onClick={() => {
+        swipeRight()
+      }} className="right-arrow absolute h-full flex flex-col items-center justify-center w-[15%] top-0 right-0">
+        <i className="fa-solid fa-arrow-right text-white text-5xl align-middle"></i>
+      </div>
+    </div>
   );
 }
