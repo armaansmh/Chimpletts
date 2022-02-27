b9b5901ca16b789a767da33208d1f21ffbfa7327
Armaan <anthracene9@protonmail.com>
2022-02-27 20:37:51 +0530

    please

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -9,190 +9,42 @@ import Image5 from "../Assets/NFTs/5.png";
 import Image6 from "../Assets/NFTs/6.png";
 import Banner from "../Assets/banner.png";
 import Image7 from "../Assets/NFTs/7.png";
+import Image339 from "../Assets/NFTs/339.png";
 import "./Carousel.scss";
 
 export default function Carousel() {
-  const [currentIndex, setIndex] = useState(0);
-  const currentImage = useRef(null);
-  const currentImage2 = useRef(null);
-  useEffect(() => {
-    /*--------------------
-Vars
---------------------*/
-    const $menu = document.querySelector(".menu");
-    const $items = document.querySelectorAll(".menu--item");
-    const $images = document.querySelectorAll(".menu--item img");
-    let menuWidth = $menu.clientWidth;
-    let itemWidth = $items[0].clientWidth;
-    let wrapWidth = $items.length * itemWidth;
-
-    let scrollSpeed = 0;
-    let oldScrollY = 0;
-    let scrollY = 0;
-    let y = 0;
-
-    /*--------------------
-Lerp
---------------------*/
-    const lerp = (v0, v1, t) => {
-      return v0 * (1 - t) + v1 * t;
-    };
-
-    /*--------------------
-Dispose
---------------------*/
-    const dispose = (scroll) => {
-      gsap.set($items, {
-        x: (i) => {
-          return i * itemWidth + scroll;
-        },
-        modifiers: {
-          x: (x, target) => {
-            const s = gsap.utils.wrap(
-              -itemWidth,
-              wrapWidth - itemWidth,
-              parseInt(x)
-            );
-            return `${s}px`;
-          },
-        },
-      });
-    };
-    dispose(0);
-
-    /*--------------------
-Wheel
---------------------*/
-    const handleMouseWheel = (e) => {
-      scrollY -= e.deltaY * 0.9;
-    };
-
-    /*--------------------
-Touch
---------------------*/
-    let touchStart = 0;
-    let touchX = 0;
-    let isDragging = false;
-    const handleTouchStart = (e) => {
-      touchStart = e.clientX || e.touches[0].clientX;
-      isDragging = true;
-      $menu.classList.add("is-dragging");
-    };
-    const handleTouchMove = (e) => {
-      if (!isDragging) return;
-      touchX = e.clientX || e.touches[0].clientX;
-      scrollY += (touchX - touchStart) * 2.5;
-      touchStart = touchX;
-    };
-    const handleTouchEnd = () => {
-      isDragging = false;
-      $menu.classList.remove("is-dragging");
-    };
-
-    /*--------------------
-Listeners
---------------------*/
-    $menu.addEventListener("mousewheel", handleMouseWheel);
-
-    $menu.addEventListener("touchstart", handleTouchStart);
-    $menu.addEventListener("touchmove", handleTouchMove);
-    $menu.addEventListener("touchend", handleTouchEnd);
-
-    $menu.addEventListener("mousedown", handleTouchStart);
-    $menu.addEventListener("mousemove", handleTouchMove);
-    $menu.addEventListener("mouseleave", handleTouchEnd);
-    $menu.addEventListener("mouseup", handleTouchEnd);
-
-    $menu.addEventListener("selectstart", () => {
-      return false;
-    });
-
-    /*--------------------
-Resize
---------------------*/
-    window.addEventListener("resize", () => {
-      menuWidth = $menu.clientWidth;
-      itemWidth = $items[0].clientWidth;
-      wrapWidth = $items.length * itemWidth;
-    });
-
-    /*--------------------
-Render
---------------------*/
-    const render = () => {
-      requestAnimationFrame(render);
-      y = lerp(y, scrollY, 0.1);
-      dispose(y);
-
-      scrollSpeed = y - oldScrollY;
-      oldScrollY = y;
-
-      gsap.to($items, {
-        skewX: -scrollSpeed * 0.2,
-        rotate: scrollSpeed * 0.01,
-        scale: 1 - Math.min(100, Math.abs(scrollSpeed)) * 0.003,
-      });
-    };
-    render();
-  }, []);
-  const swipeLeft = () => {
-    if (currentIndex == 6) {
-      gsap
-        .to(currentImage.current, { opacity: 0 })
-        .duration(0.4)
-        .then(() => {
-          setIndex(0);
-          gsap.to(currentImage.current, { opacity: 1 });
-        });
-    } else {
-      gsap
-        .to(currentImage.current, { opacity: 0 })
-        .duration(0.4)
-        .then(() => {
-          setIndex(currentIndex + 1);
-          gsap.to(currentImage.current, { opacity: 1 });
-        });
-    }
-  };
-  const swipeRight = () => {
-    if (currentIndex == 0) {
-      gsap
-        .to(currentImage2.current, { opacity: 0 })
-        .duration(0.4)
-        .then(() => {
-          setIndex(6);
-          gsap.to(currentImage2.current, { opacity: 1 });
-        });
-    } else {
-      gsap
-        .to(currentImage2.current, { opacity: 0 })
-        .duration(0.4)
-        .then(() => {
-          setIndex(currentIndex - 1);
-          gsap.to(currentImage2.current, { opacity: 1 });
-        });
-    }
-  };
+  // useEffect(() => {
+  //   document.querySelectorAll(".image-elem").forEach((image, index)=> {
+  //     image.height = document.querySelector(".something").style.height;
+  //   })
+  // })
   const [images, setImages] = useState([
     Image1,
     Image2,
     Image3,
     Image4,
-    Image5,
-    Image6,
-    Image7,
   ]);
   return (
-    <div className="flex w-[100%] relative h-[60vh] overflow-x-hidden">
-      <div className="flex menu w-[100vw] h-full">
-        {images.map((image, index) => {
-          return (
-            <div className="menu--item flex flex-col relative image-active h-full" key={index}>
-              <div className="overlay absolute top-0 left-0 w-full h-full"></div>
-              <img src={image} className="max-w-[100%] h-full w-full" alt="" />
-            </div>
-          );
-        })}
+    <div className="marquee relative h-[50vh] w-[100vw] overflow-hidden">
+      <div className="marquee--inner absolute block shrink-0 remove-scroll w-[200%] bg-black">
+        <span className="flex w-[50%] float-left something relative ">
+          {images.map((image, key) => (
+            <img
+              src={image}
+              className="image-elem"
+              alt=""
+            />
+          ))}
+        </span>
+        <span className="flex w-[50%] float-left something relative ">
+          {images.map((image, key) => (
+            <img
+              src={image}
+              className="image-elem"
+              alt=""
+            />
+          ))}
+        </span>
       </div>
     </div>
   );
